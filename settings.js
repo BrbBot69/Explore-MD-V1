cfc281bd74fadfbd2ab88e16e33afe9673311e94
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-06 08:10:42 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'madewith❤️;DFS',
-  author: 'reddragon-dfs',
-  botName: "explore md",
-  botOwner: 'Reddragon-DFS', // Your name
-  ownerNumber: '27634988678', //Set your number here without + symbol.
+  packname: '~M@inly_M€',
+  author: '~M@inly_M€',
+  botName: "ⒶⓋⒺ~ⓂⒹ",
+  botOwner: '~M@inly_M€', // Your name
+  ownerNumber: '27767494368', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
